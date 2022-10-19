ad81cb057c560089400c41bdbda52cf42efd8df5
kubo-hide-kun <hide44041@gmail.com>
2022-10-20 01:30:04 +0900

    feat(@conken-oss-pkg/frontend-react-emotion): build a storybook enbironment :sparkles:

--- packages/frontend-react-emotion/.storybook/main.js
+++ packages/frontend-react-emotion/.storybook/main.js
@@ -1,7 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  stories: ['../src/stories/**/*.stories.(ts|tsx)'],
+  stories: ['../src/**/*.stories.(ts|tsx)'],
+  core: {
+    builder: "webpack5",
+  },
   addons: [
     '@storybook/addon-actions',
     '@storybook/addon-links',
