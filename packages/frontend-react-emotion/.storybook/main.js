5b8157668948973aa2dea994e456cc196cdeb5ab
kubo-hide-kun <hide44041@gmail.com>
2022-10-21 22:20:43 +0900

    feat(@conken-oss-pkg/frontend-react-emotion): add support for storybook :sparkles:

--- packages/frontend-react-emotion/.storybook/main.js
+++ packages/frontend-react-emotion/.storybook/main.js
@@ -1,37 +1,12 @@
-const path = require('path');
-
 module.exports = {
-  stories: ['../src/**/*.stories.(ts|tsx)'],
-  core: {
-    builder: "webpack5",
-  },
-  addons: [
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-    {
-      name: '@storybook/preset-create-react-app',
-      options: {
-        tsDocgenLoaderOptions: {
-          tsconfigPath: path.resolve(__dirname, '../tsconfig.json')
-        }
-      }
-    },
-    {
-      name: '@storybook/addon-docs',
-      options: {
-        configureJSX: true
-      }
-    }
+  stories: [
+    "../**/*.stories.mdx",
+    "../**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  webpackFinal: (config) => {
-    config.module.rules.push({
-      test: /\.tsx$/,
-      loader: require.resolve('babel-loader'),
-      options: {
-        cacheDirectory: true,
-        presets: [require.resolve('@emotion/babel-preset-css-prop')],
-      },
-    });
-    return config;
-  },
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  framework: "@storybook/react",
+  babel: async (options) => ({
+    ...options,
+    presets: [...options.presets, "@emotion/babel-preset-css-prop"],
+  }),
 };
