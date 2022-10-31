08993b35f15d9cdc2011cf7181db454ac93a2fa9
kubo-hide-kun <hide44041@gmail.com>
2022-10-31 22:25:04 +0900

    feat(all): add support to use simple scope :sparkles:

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,11 +1,30 @@
+const path = require("path");
+const fs = require("fs");
+
+const extractDirectoryNames = (path, { ignoreHiddenFiles = true } = {}) => {
+  return fs
+    .readdirSync(path, {
+      withFileTypes: true,
+    })
+    .filter((dirent) => dirent.isDirectory)
+    .filter((dirent) => !ignoreHiddenFiles || !dirent.name.startsWith("."))
+    .map((dirent) => dirent.name);
+};
+
+// Note: apps ディレクトリと packages ディレクトリの配下のディレクトリ名を取得
+// Note: この処理により、スコープを指定する際に @conken-oss-pkgs/ の部分を省略できる
+const targetDirectories = [
+  path.resolve(__dirname, "apps"),
+  path.resolve(__dirname, "packages"),
+].map((dir) => extractDirectoryNames(dir)).flat();
+
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   plugins: [
     'scope-empty-exceptions', // scope が空の場合にエラーを出す
-    'workspace-scopes' // workspace のパッケージ名を scope として許可する
   ],
   rules: {
     'scope-empty': [2, 'never', []],
-    'scope-enum': [2, 'always', ['all']], // scope が all の場合も許可する
+    'scope-enum': [2, 'always', ['all', ...targetDirectories]], // scope が all の場合も許可する
   },
 };
