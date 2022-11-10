47eee94efef08368ec843bd741849e35aabe724b
kubo-hide-kun <hide44041@gmail.com>
2022-11-11 00:36:14 +0900

    feat(services): create Id value object :sparkles:

---
+++ packages/services/src/shared/domains/valueObject/id.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+export const idSchema = z.string().uuid();
+
+export type IdValue = z.infer<typeof idSchema>;
+
+export class Id {
+  constructor(public readonly value: IdValue) {
+    idSchema.parse(value);
+    Object.freeze(this);
+  }
+
+  toString(): string {
+    return this.value;
+  }
+}
