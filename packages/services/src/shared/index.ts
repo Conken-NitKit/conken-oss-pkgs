5c6d95943cd9d011b2d4694007d1dd89c4bc232d
kubo-hide-kun <hide44041@gmail.com>
2022-11-13 02:42:31 +0900

    refactor(services): remove unused namespace :recycle:

--- packages/services/src/shared/index.ts
+++ packages/services/src/shared/index.ts
@@ -1,21 +1,14 @@
-import { Result as _Result } from "~/core/Result";
-import { ValueObject as _ValueObject } from "~/core/ValueObject";
-import { Id as _Id, IdValue as _IdValue } from "~/shared/valueObjects/Id";
-
-/**
- * アーキテクチャを構成する要素のうち、特定のレイヤーに依存しないものを定義する
- */
-export namespace Core {
-  export const Result = _Result;
-  export const ValueObject = _ValueObject;
-}
+import {
+  Id as SharedId,
+  IdValue as SharedIdValue,
+} from "~/shared/valueObjects/Id";
 
 export namespace Shared {
   /**
    * 値オブジェクトを定義する
    */
   export namespace ValueObjects {
-    export type IdValue = _IdValue;
-    export const Id = _Id;
+    export const Id = SharedId;
+    export type IdValue = SharedIdValue;
   }
 }
