7d455fdd9e53719af7fd8fae39115495b182786f
kubo-hide-kun <hide44041@gmail.com>
2022-11-12 20:00:32 +0900

    refactor(services): auto format :recycle:

--- packages/services/src/shared/core/valueObject.ts
+++ packages/services/src/shared/core/valueObject.ts
@@ -3,7 +3,10 @@ export type Validator<T> = (value: T) => { success: boolean };
 export abstract class ValueObject<T> {
   abstract validator: Validator<T>;
 
-  constructor(protected readonly value: T, protected readonly objectName: string) {
+  constructor(
+    protected readonly value: T,
+    protected readonly objectName: string
+  ) {
     const { success } = this.validate();
     if (!success) {
       throw new Error(`Invalid ${this.objectName}`);
