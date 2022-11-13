852bd92dac54cf661c861570a9cc3578fd06ed52
kubo-hide-kun <hide44041@gmail.com>
2022-11-13 22:52:56 +0900

    refactor(services): update name of test case :recycle:

--- packages/services/src/shared/valueObjects/Id/index.spec.ts
+++ packages/services/src/shared/valueObjects/Id/index.spec.ts
@@ -1,14 +1,14 @@
 import { Shared } from "~/shared";
 
 describe("ID のバリデーションが適切である。", () => {
-  const TEST_DATA = [
+  const TEST_CASES = [
     { id: "E034B584-7D89-11E9-9669-1AECF481A97B", expected: true },
     { id: "A987FBC9-4BED-4078-8F07-9141BA07C9F3", expected: true },
     { id: "AAAAAAAA-1111-2222-AAAG-111111111111", expected: false },
     { id: "xxxA987FBC9-4BED-3078-CF07-9141BA07C9F3", expected: false },
     { id: "AAAAAAAA-1111-2222-AAAG", expected: false },
   ];
-  TEST_DATA.forEach((data) => {
+  TEST_CASES.forEach((data) => {
     it(`${data.expected ? "有効" : "無効"}なID: ${data.id}`, () => {
       const id = Shared.ValueObjects.Id.create(data.id, `ID: ${data.id}`);
       expect(id.success).toBe(data.expected);
