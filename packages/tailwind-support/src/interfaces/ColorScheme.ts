b09d561912b25fad62cdebaa22e11dbf2719d5c9
kubo-hide-kun <hide44041@gmail.com>
2022-11-25 17:41:54 +0900

    feat(tailwind-support): create interface of color scheme :sparkles:

---
+++ packages/tailwind-support/src/interfaces/ColorScheme.ts
@@ -0,0 +1,55 @@
+type TextColorScheme = {
+  high_emphasis: string;
+  medium_emphasis: string;
+  low_emphasis: string;
+  accent_primary: string;
+  accent_secondary: string;
+  success: string;
+  warning: string;
+  disable: string;
+  caution: string;
+  hint: string;
+};
+
+type BackgroundColorScheme = {
+  primary: string;
+  secondary: string;
+  tertiary: string;
+  accent_primary: string;
+  accent_secondary: string;
+  success: string;
+  warning: string;
+  disable: string;
+  caution: string;
+  overlay: string;
+  hint: string;
+};
+
+/**
+ * Note: xxx_gradient_xxx の形式のキーを定義してください。
+ */
+type BackgroundGradientColorScheme = {
+  accent_gradient_primary: [string, string];
+  accent_gradient_secondary: [string, string];
+};
+
+type BorderColorScheme = {
+  high_emphasis: string;
+  medium_emphasis: string;
+  low_emphasis: string;
+  success: string;
+  warning: string;
+  DISABLE: string;
+  caution: string;
+  hint: string;
+};
+
+export type ColorScheme = {
+  text: TextColorScheme;
+  text_inverted: TextColorScheme;
+  background: BackgroundColorScheme;
+  background_inverted: BackgroundColorScheme;
+  background_gradient: BackgroundGradientColorScheme;
+  border: BorderColorScheme;
+  border_inverted: BorderColorScheme;
+};
